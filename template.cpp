aee5011917a6e9d15dfff51ce14ddb242b4706e1
Mohit <joshimj143@gmail.com>
2022-01-20 23:33:42 +0530

    added macros

--- template.cpp
+++ template.cpp
@@ -1,9 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void printArray(int arr[], int size){
+#define fl(i, a, b) for (int i = a; i < b; i++)
+#define piv(a) printf("%d",a)
+#define pis(s) printf("%s",s)
+#define siv(a) scanf("%d",a)
+
+typedef long long int lli;
+typedef vector<int> vi;
+
+void printArray(int *arr, int size){
     for (int i = 0; i < size; i++)
         cout << arr[i] << " ";
+    cout << endl;
+} 
+void printVector(vector<int>& vec){
+    for (int i = 0; i < vec.size(); i++)
+        cout << vec[i] << " ";
     cout << endl;
 }
 
