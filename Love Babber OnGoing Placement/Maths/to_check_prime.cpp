6ddd08c1ad83cdb34b6fae6daa2a83447c146e31
Mohit <joshimj143@gmail.com>
2022-01-28 18:25:32 +0530

    updated

--- Love Babber OnGoing Placement/Maths/to_check_prime.cpp
+++ Love Babber OnGoing Placement/Maths/to_check_prime.cpp
@@ -5,7 +5,7 @@ bool isPrime(int n){
         return false;
     if(n==2 || n==3)
         return true;
-    for(int i = 1; i < n-2;i++){
+    for(int i = 1; i < n-2;i++){  // any prime no is in the form of 6n-1 or 6n+1 except 2 and 3
         if(6*i == n-1 || 6*i == n+1)
             return true;
     }
