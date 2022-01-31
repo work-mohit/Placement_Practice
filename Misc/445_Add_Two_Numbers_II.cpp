6584c1d5f308bd20069a4378b416b6549f46a160
Mohit <joshimj143@gmail.com>
2022-01-31 23:43:07 +0530

    added 445

---
+++ Misc/445_Add_Two_Numbers_II.cpp
@@ -0,0 +1,36 @@
+class Solution {
+
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        stack<int> stk1,stk2;
+        ListNode* p1 = l1;
+        
+        while(p1!= NULL){
+            stk1.push(p1->val);
+            p1 = p1->next;
+        }
+        ListNode* p2 = l2;
+        while(p2!=NULL){
+            stk2.push(p2->val);
+            p2 = p2->next;
+        }
+        
+        int summ,carry = 0;
+        ListNode *prev = NULL; 
+        while(!stk1.empty() || !stk2.empty() || carry){
+            
+            summ = (stk1.empty()?0:stk1.top()) + (stk2.empty()?0:stk2.top()) + carry;
+            carry = summ / 10;
+            
+            ListNode *curr = new ListNode(summ%10,prev);  // check the declaration of the ListNode
+            prev = curr;
+            
+            if(!stk1.empty())stk1.pop();
+            if(!stk2.empty())stk2.pop();
+        }
+        
+        
+      return prev; 
+        
+    }
+}; 
