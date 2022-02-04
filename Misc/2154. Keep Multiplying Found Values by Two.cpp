8bf34fd0111c7c2517701aa29a6fb185af067492
Mohit <joshimj143@gmail.com>
2022-02-04 22:35:44 +0530

    added 2154

---
+++ Misc/2154. Keep Multiplying Found Values by Two.cpp
@@ -0,0 +1,28 @@
+class Solution {
+
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+         while(original<=1000)
+        {
+            int cnt=0;
+           for(int i=0;i<nums.size();i++)
+           {
+               if(nums[i]==original)
+               {
+                   original=2*original;
+                   break;
+               }
+               else
+               {
+                   cnt++;
+               }
+           }
+            if(cnt==nums.size())
+            {
+                return original;
+            }
+        }
+        return original;
+    }
+    
+};
