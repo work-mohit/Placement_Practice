ee19c4b53c260e3c5105fbfa1afd154fff31ee65
Mohit <joshimj143@gmail.com>
2022-02-06 20:43:11 +0530

    added 2161

---
+++ LeetCode Contest/BiWeekly 71/2161. Partition Array According to Given Pivot.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/partition-array-according-to-given-pivot/
+class Solution {
+
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        
+        vector<int> ans;
+        for(int x:nums){
+            if(x<pivot){
+                ans.push_back(x);
+            }
+        }
+        for(int x:nums){
+            if(x==pivot){
+                ans.push_back(x);
+            }
+        }
+        for(int x:nums){
+            if(x>pivot){
+                ans.push_back(x);
+            }
+        }
+        return ans;
+  
+    }
+};
