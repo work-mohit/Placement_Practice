9899b9b0d54e3ade5a62f9dd7652aa61cae11900
Mohit <joshimj143@gmail.com>
2022-02-18 23:03:59 +0530

    added

---
+++ Love Babber OnGoing Placement/Queue/Sum of minimum and maximum elements of all subarrays of size k.cpp
@@ -0,0 +1,15 @@
+/*
+https://www.geeksforgeeks.org/sum-minimum-maximum-elements-subarrays-size-k/
+
+Input : arr[] = {2, 5, -1, 7, -3, -1, -2}  
+        K = 4
+Output : 18
+Explanation : Subarrays of size 4 are : 
+     {2, 5, -1, 7},   min + max = -1 + 7 = 6
+     {5, -1, 7, -3},  min + max = -3 + 7 = 4      
+     {-1, 7, -3, -1}, min + max = -3 + 7 = 4
+     {7, -3, -1, -2}, min + max = -3 + 7 = 4   
+     Sum of all min & max = 6 + 4 + 4 + 4 
+                          = 18  
+*/
+
