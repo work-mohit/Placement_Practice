cac8286ef76e440c8b9e036b65432cda02c40e44
Mohit <joshimj143@gmail.com>
2022-02-19 12:46:47 +0530

    added

--- Love Babber OnGoing Placement/Queue/Sum of minimum and maximum elements of all subarrays of size k.cpp
+++ Love Babber OnGoing Placement/Queue/Sum of minimum and maximum elements of all subarrays of size k.cpp
@@ -13,3 +13,53 @@ Explanation : Subarrays of size 4 are :
                           = 18  
 */
 
+long long sumOfMaxAndMin(vector<int> &arr, int n, int k) {
+	deque<int> S, G;
+    long long sum = 0;
+    
+    // Process first window of size K
+    int i = 0;
+    for (i = 0; i < k; i++)
+    {
+        // Remove all previous greater elements
+        // that are useless.
+        while ( (!S.empty()) && arr[S.back()] >= arr[i])
+            S.pop_back(); // Remove from rear
+ 
+        // Remove all previous smaller that are elements
+        // are useless.
+        while ( (!G.empty()) && arr[G.back()] <= arr[i])
+            G.pop_back(); // Remove from rear
+ 
+        // Add current element at rear of both deque
+        G.push_back(i);
+        S.push_back(i);
+    }
+    
+    // now we greatest and smallest element in their respective queue
+    
+    for( ; i< n; i++){
+        sum += arr[S.front()] + arr[G.front()];
+        // now remove all previous greater element that are now out of index
+        while(!S.empty() && S.front() <= i-k){
+            S.pop_front();
+        }
+        while(!G.empty() && G.front() <= i-k){
+            G.pop_front();
+        }
+        // now do same as we did for the first k elements
+        while ( (!S.empty()) && arr[S.back()] >= arr[i])
+            S.pop_back(); // Remove from rear
+ 
+        // remove all previous smaller that are elements
+        // are useless
+        while ( (!G.empty()) && arr[G.back()] <= arr[i])
+            G.pop_back(); // Remove from rear
+        
+        G.push_back(i);
+        S.push_back(i);
+      
+    }
+    sum += arr[S.front()] + arr[G.front()];
+    return sum ;
+}
