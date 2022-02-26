062b573bf0d364a9fa9dd7b448dfb4ab09bc3086
Mohit <joshimj143@gmail.com>
2022-02-26 23:33:02 +0530

    added more macros

--- template.cpp
+++ template.cpp
@@ -1,13 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define fl(i, a, b) for (int i = a; i < b; i++)
+#define range(i, a, n) for (int i = a; i < n; i++)
 #define piv(a) printf("%d",a)
 #define pis(s) printf("%s",s)
 #define siv(a) scanf("%d",a)
 
+#define vi vector<int>
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define all(x) x.begin(),x.end()
+#define vb vector<bool>
+#define vvl vector<vector<int> >
+#defind umap unordered_map
+#define mp make_pair
 typedef long long int lli;
-typedef vector<int> vi;
+
 
 void printArray(int *arr, int size){
     for (int i = 0; i < size; i++)
@@ -20,10 +27,10 @@ void printVector(vector<int>& vec){
     cout << endl;
 }
 
+
+
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    
     int t;
     cin >> t;
     while(t--){
