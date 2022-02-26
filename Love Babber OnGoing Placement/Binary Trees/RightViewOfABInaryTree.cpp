7df2b1ef4ea9d0825746b33a4bae5bff8cc8d27a
Mohit <joshimj143@gmail.com>
2022-02-26 23:32:51 +0530

    updated

--- Love Babber OnGoing Placement/Binary Trees/RightViewOfABInaryTree.cpp
+++ Love Babber OnGoing Placement/Binary Trees/RightViewOfABInaryTree.cpp
@@ -4,22 +4,24 @@ class Solution
     void helper(Node* root, vector<int> &ans, int lvl){
         if(root == NULL)
             return ;
-        if(lvl == ans.size())
-            ans.push_back(root->data);
-        helper(root->right,ans, lvl+1);
+        // at this point we are checking the size of
+        // vector let say ans size is 2 means we have gone till 2 level and  
+        // now if we get into 3rd level then only we push element in the vector
+        if(lvl == ans.size())     
+            ans.push_back(root->data);  
+                                         
+        helper(root->right,ans, lvl+1);  
         helper(root->left,ans, lvl+1);
         
     }
     //Function to return list containing elements of right view of binary tree.
     vector<int> rightView(Node *root)
     {
-       // Your Code here
-       vector<int> ans;
+        // Your Code here
+        vector<int> ans;
         
         helper(root,ans, 0);
         return ans;
     }
-    
-    
    
 };
