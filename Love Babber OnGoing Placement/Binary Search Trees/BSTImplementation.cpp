44b28f3977f740c6894bc003d3cc73dd8e186d04
Mohit <joshimj143@gmail.com>
2022-03-09 10:00:51 +0530

    added BST Implementation

--- Love Babber OnGoing Placement/Binary Search Trees/BSTImplementation.cpp
+++ Love Babber OnGoing Placement/Binary Search Trees/BSTImplementation.cpp
@@ -42,10 +42,61 @@ void inorderTraversal(Node* root){
     cout<<root->data<<" ";
     inorderTraversal(root->right);
 }
+Node* minVal(Node* root){
+    Node* temp = root;
+    while(temp->left != NULL){
+        temp = temp -> left;
+    }
+    return root;
+}
+Node* deleteFromBST(Node* root, int x){
+    // base case 
+    if(root == NULL)
+        return root;
+
+    if(root->data == x){
+        // 0 child
+        if(root->left == NULL  && root->right == NULL){
+            delete root;
+            return NULL;
+        }
+         // 1 left child
+        if(root->left != NULL && root->right == NULL){
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        // 1 right child
+        if(root->left == NULL && root->right != NULL){
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+        // 2 child
+        if(root->left != NULL && root->right != NULL)
+            Node* mini = minVal(root->right);  // get min value form the right subtree.
+            root->data = mini->data;
+            root->right = deleteFromBST(root->right, mini);
+            // return root;
+        }
+    }
+    else if(root->data > x){
+        root->left = deleteFromBST(root->left , x);    
+    }
+    else{
+        root->right = deleteFromBST(root->right, x);
+        
+    }
+    return root;
+}
 
 int main(){
     Node* root = NULL;
     takeInput(root);
     inorderTraversal(root);
+    cout<<endl;
+    cout<<"Delete node:\n";
+    deleteFromBST(root,25);
+    cout<<minVal;
     return 0;
 }
