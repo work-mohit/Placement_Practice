2bf4869ad9bb76bfa690de14772f73761bf69cef
Mohit <joshimj143@gmail.com>
2022-03-17 13:32:53 +0530

    added kth largest subarray sum

---
+++ Love Babber OnGoing Placement/Heaps/Kth largest Subarray Sum.cpp
@@ -0,0 +1,38 @@
+int getKthLargest(vector<int> &arr, int k)
+{
+// 	priority_queue<int, vector<int> ,greater<int>> pq;
+    vector<int> ans;
+    for(int i =0 ; i< arr.size(); i++){
+        int sum = 0;
+        for(int j = i; j < arr.size();j++){
+            sum += arr[j];
+            ans.push_back(sum);
+        }  
+    }
+    sort(ans.begin(), ans.end());
+    return ans[ans.size() - k];
+}
+
+//optimal approach
+
+#include<bits/stdc++.h>
+int getKthLargest(vector<int> &arr, int k)
+{
+	priority_queue<int, vector<int> ,greater<int>> pq;
+    
+    for(int i =0 ; i< arr.size(); i++){
+        int sum = 0;
+        for(int j = i; j < arr.size();j++){
+            sum += arr[j];
+            if(pq.size()< k){
+                pq.push(sum);
+            }else{
+                if(sum > pq.top()){
+                    pq.pop();
+                    pq.push(sum);
+                }
+            }
+        }  
+    }
+    return pq.top();
+}
