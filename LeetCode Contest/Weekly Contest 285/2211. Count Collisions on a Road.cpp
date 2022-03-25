d6819641f4ebe74a83e27e7f98efc64b13d457a3
Mohit <joshimj143@gmail.com>
2022-03-25 21:52:01 +0530

    added 2210 & 2211

---
+++ LeetCode Contest/Weekly Contest 285/2211. Count Collisions on a Road.cpp
@@ -0,0 +1,28 @@
+//  
+class Solution {
+    public int countCollisions(String directions) {
+        int left = 0;
+        int right = directions.length() - 1;
+        int cnt = 0;
+
+        // left side me phle jitne bhi left ko jari unhe hta do 
+        // unka koi contribution nhi 
+        while(left <= right && directions.charAt(left) == 'L'){
+            left++;
+        }
+
+        // same right side se jo R ko jaari phle .. un sb ko hta do 
+        while(right >= 0 && directions.charAt(right) == 'R'){
+            right--;
+        }
+        
+        // ab jo bache kuche bache usme stationary car ho ya na ho baat brabar hi hai 
+        // toh bs count krlo kitne left and right hai total 
+        for(int i = left; i<=right; i++){
+            if(directions.charAt(i) != 'S'){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
