c99783ffb38ffe2a938a7021b2aeacd2b2b3034d
Mohit <joshimj143@gmail.com>
2022-03-25 18:24:33 +0530

    added quick select method and nth element method

--- Love Babber OnGoing Placement/Heaps/Kth Smallest Element.cpp
+++ Love Babber OnGoing Placement/Heaps/Kth Smallest Element.cpp
@@ -36,3 +36,87 @@ class Solution{
 
 //////
 // quick select method avg O(n) TC
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    
+    int partition(int a[], int left, int right, int pIndex)
+    {
+        // pick `pIndex` as a pivot from the array
+        int pivot = a[pIndex];
+     
+        // Move pivot to end
+        swap(a[pIndex], a[right]);
+     
+        // elements less than the pivot will be pushed to the left of `pIndex`;
+        // elements more than the pivot will be pushed to the right of `pIndex`;
+        // equal elements can go either way
+        pIndex = left;
+     
+        // each time we find an element less than or equal to the pivot, `pIndex`
+        // is incremented, and that element would be placed before the pivot.
+        for (int i = left; i < right; i++)
+        {
+            if (a[i] <= pivot)
+            {
+                swap(a[i], a[pIndex]);
+                pIndex++;
+            }
+        }
+     
+        // move pivot to its final place
+        swap(a[pIndex], a[right]);
+     
+        // return `pIndex` (index of the pivot element)
+        return pIndex;
+    }
+    int quickselect(int nums[], int left, int right, int k)
+    {
+        // If the array contains only one element, return that element
+        if (left == right) {
+            return nums[left];
+        }
+     
+        // select `pIndex` between left and right
+        int pIndex = left + rand() % (right - left + 1);
+     
+        pIndex = partition(nums, left, right, pIndex);
+     
+        // The pivot is in its final sorted position
+        if (k == pIndex) {
+            return nums[k];
+        }
+     
+        // if `k` is less than the pivot index
+        else if (k < pIndex) {
+            return quickselect(nums, left, pIndex - 1, k);
+        }
+     
+        // if `k` is more than the pivot index
+        else {
+            return quickselect(nums, pIndex + 1, right, k);
+        }
+    }
+    int kthSmallest(int arr[], int left, int right, int k) {
+        return quickselect(arr, left, right, k-1);
+    }
+    
+    
+};
+
+///////////////////
+// using method called nth element 
+int main()
+{
+    vector<int> a = { 7, 4, 6, 3, 9, 1 };
+    const size_t k = 2;
+ 
+    nth_element(a.begin(), a.begin() + k, a.end());
+    cout << "k'th smallest element is " << a[k];
+ 
+    return 0;
+}
